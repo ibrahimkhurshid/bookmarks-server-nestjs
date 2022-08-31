e0721fb7acfc16737cce08f30294110f093328bb
Ibrahim Khurshid <ibrahimkhurshid@hotmail.com>
2022-08-31 17:56:27 +0500

    swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { AppModule } from './app.module';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmarks Api')
+    .setDescription('The Crud Api for bookmark resource')
+    .addTag('Bookmarks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
